964176f32f7d534a34cc14697ae3ec3ad3ec7973
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-14 23:41:19 -0300

    add: db persistance - saving db file

--- src/database.js
+++ src/database.js
@@ -1,5 +1,26 @@
+import fs from "node:fs/promises"
+
+// using type="module" in package.json, we can't use __dirname
+const databasePath = new URL("../db.json", import.meta.url)
+// if we don't specify the path, it will be created where the script is running
+
 export class Database {
   #database = {}
+  // # is a private property
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,6 +34,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
